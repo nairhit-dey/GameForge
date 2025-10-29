852a78338a4b8acf9ac60506d980c5421fb10f8d
nairhit-dey <ndeybb705@gmail.com>
2025-10-29 22:44:32 +0530

    Add one new product and image

--- products.ts
+++ products.ts
@@ -84,6 +84,13 @@ export const productsData: Product[] = [
     price: 129.99,
     image: 'https://images.pexels.com/photos/325153/pexels-photo-325153.jpeg?auto=compress&cs=tinysrgb&w=800',
     description: 'Customizable control panel with LCD keys for streaming'
+  },
+  {
+    id: '8',
+    name: 'Mechanical Numpad',
+    price: 59.99,
+    image: 'https://images.pexels.com/photos/4551315/pexels-photo-4551315.jpeg?auto=compress&cs=tinysrgb&w=800',
+    description: 'Programmable mechanical numpad with hot-swappable switches'
   }
 ];
 
